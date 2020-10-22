0af1d12047ef32ec2d69233932952df8f1bb38bb
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-22 17:18:34 +0530

    added boxform component and removing of boxes function

--- src/Box.js
+++ src/Box.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Box extends Component {
-    render() {
-        return (
-            <div>
-                <div style={{
-                    height: `${this.props.height}em`,
-                    width: `${this.props.width}em`,
-                    backgroundColor: this.props.color
-                }}
-                />
-                <button>X</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            height: `${this.props.height}em`,
+            width: `${this.props.width}em`,
+            backgroundColor: this.props.color
+          }}
+        />
+        <button onClick={this.props.removeBox}>X</button>
+      </div>
+    );
+  }
 }
-
 export default Box;
-
